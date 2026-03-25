d05bb0c0b7173f72c38919acb3f7000dc0e914d5
Yeonnies <126739852+Yeonnies@users.noreply.github.com>
2026-03-26 03:22:02 +0900

    [Setting] #1 - First Setting

---
+++ ToDivers-iOS/ToDivers-iOS/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  ToDivers-iOS
+//
+//  Created by Neon on 3/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                colors: [
+                    Color.blue.opacity(0.6),
+                    Color.indigo.opacity(0.8)
+                ],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            
+            Color.white
+                .opacity(0.5)
+                .blur(radius: 30)
+                .ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    OceanView()
+}
---
+++ ToDivers-iOS/ToDivers-iOS/ToDivers_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDivers_iOSApp.swift
+//  ToDivers-iOS
+//
+//  Created by Neon on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDivers_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
