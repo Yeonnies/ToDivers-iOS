190a32bb62e4784c5292f951dfa03744d7cdccb3
Yeonnies <126739852+Yeonnies@users.noreply.github.com>
2026-03-26 11:12:05 +0900

    [Feat] #1 - 파도 구현

--- ToDivers-iOS/ToDivers-iOS/ContentView.swift
+++ ToDivers-iOS/ToDivers-iOS/ContentView.swift
@@ -9,26 +9,82 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var progress: CGFloat = 0.5
+    @State var startAnimation: CGFloat = 0
+    
     var body: some View {
-        ZStack {
-            LinearGradient(
-                colors: [
-                    Color.blue.opacity(0.6),
-                    Color.indigo.opacity(0.8)
-                ],
-                startPoint: .top,
-                endPoint: .bottom
-            )
+        GeometryReader { geo in
+            ZStack {
+                LinearGradient(
+                    colors: [
+                        Color.blue.opacity(0.6),
+                        Color.indigo.opacity(0.8)
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                
+                Color.white
+                    .opacity(0.5)
+                    .blur(radius: 30)
+                
+                WaterWave(progress: 0.5, waveHeight: 0.05, offset: startAnimation)
+                    .fill(
+                        LinearGradient(
+                            colors: [
+                                Color.blue.opacity(0.6),
+                                Color.indigo.opacity(0.8)
+                            ],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                    )
+                    .overlay {
+                        Color.white
+                            .opacity(0.5)
+                            .blur(radius: 30)
+                    }
+            }
             .ignoresSafeArea()
-            
-            Color.white
-                .opacity(0.5)
-                .blur(radius: 30)
-                .ignoresSafeArea()
+            .onAppear {
+                withAnimation(.linear(duration: 2).repeatForever(autoreverses: false)){
+                    startAnimation = geo.size.width
+                }
+            }
         }
     }
 }
 
-#Preview {
-    OceanView()
+struct WaterWave: Shape {
+    var progress: CGFloat
+    var waveHeight: CGFloat
+    var offset: CGFloat
+    var animatableData: CGFloat {
+        get {offset}
+        set {offset = newValue}
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        return Path { path in
+            path.move(to: .zero)
+            
+            let progressHeight: CGFloat = (1 - progress) * rect.height
+            let height = waveHeight * rect.height
+            
+            for value in stride(from: 0, to: rect.width, by: 2) {
+                let x: CGFloat = value
+                let sine: CGFloat = sin(Angle(degrees: value + offset).radians)
+                let y: CGFloat = progressHeight + (height * sine)
+                
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+            
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: rect.height))
+        }
+    }
 }
+
+//#Preview {
+//    ContentView()
+//}
