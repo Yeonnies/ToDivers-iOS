4000ea8d83445394b24c0da834b6f9ed29d5daa0
Yeonnies <126739852+Yeonnies@users.noreply.github.com>
2026-04-16 12:19:47 +0900

    [Fix] #8 - 햅틱 추가

--- ToDivers-iOS/ToDivers-iOS/Feature/Dive/Haptic.swift
+++ ToDivers-iOS/ToDivers-iOS/Feature/Dive/Haptic.swift
@@ -8,7 +8,22 @@
 import SwiftUI
 
 final class Haptic {
-    static func notification() {
-        
+    private static let impactGenerators: [UIImpactFeedbackGenerator.FeedbackStyle: UIImpactFeedbackGenerator] = {
+        let styles: [UIImpactFeedbackGenerator.FeedbackStyle] = [.light, .medium, .heavy, .soft, .rigid]
+        return Dictionary(uniqueKeysWithValues: styles.map { ($0, UIImpactFeedbackGenerator(style: $0)) })
+    }()
+    
+    static func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.prepare()
+        generator.notificationOccurred(type)
+    }
+    
+    static func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        DispatchQueue.main.async {
+            let generator = UIImpactFeedbackGenerator(style: style)
+            generator.prepare()
+            generator.impactOccurred()
+        }
     }
 }
